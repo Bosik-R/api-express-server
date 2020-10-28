fc89f0aaed9c197eda93d84e01a7a19b418ae7f3
Tomasz Rosik <Bosik27@gmail.com>
2020-10-29 00:50:47 +0100

    Update server path

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
-
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const seatsRoutes = require('./routes/seats.routes');
@@ -13,11 +15,13 @@ app.use(cors());
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' }); 
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
